189a304f60d45952b3ce578250256553151df707
Alena Vasilchenko <vasilchenko.alen@yandex.ru>
2020-06-23 11:37:27 +0300

    add simple form validation

--- src/components/common/Form.js
+++ src/components/common/Form.js
@@ -4,35 +4,63 @@ import Button from "./Button";
 
 const Form = ({ columns, initialData, onFormSubmit }) => {
     const [personData, setPersonData] = useState(initialData);
+    const [formErrors, setFormErrors] = useState({});
 
     const handleSubmit = (event) => {
         event.preventDefault();
+        const errors = validate(personData);
+
+        if (Object.keys(errors).length) {
+            return;
+        }
+
         onFormSubmit(personData);
     }
 
     const handleChange = (event) => {
         const { currentTarget: input } = event;
-        const data = {...personData};
+        const data = { ...personData };
+        const errors = { ...formErrors };
+
+        if (errors[input.name]) {
+            delete errors[input.name];
+        }
+
         data[input.name] = input.value;
-        setPersonData(data)
+        setPersonData(data);
+        setFormErrors(errors);
+    }
+
+    const validate = (data) => {
+        let errors = {};
+
+        columns.map((key) => {
+            if (!data[key]) {
+                errors = { ...errors, [key]: 'Field is required' };
+            }
+        })
+
+        setFormErrors(errors);
+        return errors;
     }
 
     return (
         <form>
             {columns.map(columnName => (
                 <Input
-                key={columnName}
-                name={columnName}
-                label={columnName}
-                value={personData[columnName] || ''}
-                type="input"
-                onChange={handleChange}
+                    key={columnName}
+                    name={columnName}
+                    label={columnName}
+                    value={personData[columnName] || ''}
+                    type="input"
+                    error={formErrors[columnName]}
+                    onChange={handleChange}
                 />
             ))}
 
             <Button
                 label="Save"
-                classes="alert alert-danger"
+                classes="alert alert-success"
                 onClick={handleSubmit}
             />
         </form>
