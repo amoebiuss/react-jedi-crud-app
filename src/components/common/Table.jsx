f2b0816ccdb304e704f8af6825f732532e0cac2b
Alena Vasilchenko <vasilchenko.alen@yandex.ru>
2020-06-23 15:05:31 +0300

    modify table: hide the whole table if there is no data

--- src/components/common/Table.jsx
+++ src/components/common/Table.jsx
@@ -8,43 +8,40 @@ function Table({ columns, data, tableDescriptor, onItemDelete }) {
     let { url } = useRouteMatch();
 
     return (<>
-        <table className="table table-dark">
-            <thead>
-                <tr>
-                    <th scope="col">{tableDescriptor}</th>
-                    {columns.map(columnTitle => (
-                        <th key={columnTitle} scope="col">{columnTitle}</th>
-                    ))}
-                </tr>
-            </thead>
-            <tbody>
-                {data.map((item, index) => (
-                    <tr key={item.id} className="table-row">
-                        <th scope="row">
-                            {++index}
-                        </th>
-                        {columns.map(columnTitle => (
-                            <td key={item[columnTitle] + columnTitle}>
-                                {item[columnTitle]}
-                            </td>
-                        ))}
-                        <td className="btn-container">
-                            <Button onClick={() => onItemDelete(item.id)} label="Delete" classes='btn-danger'></Button>
-
-                            <Link to={{
-                                pathname: `${url}/${item.id}`,
-                                state: { ...item }
-                            }} style={{ margin: '0 10px' }}>
-                                Edit
-                            </Link>
-                        </td>
-                    </tr>
-                ))}
-            </tbody>
-        </table>
-
         {data.length
-            ? null
+            ? (<table className="table table-dark">
+                <thead>
+                    <tr>
+                        <th scope="col">{tableDescriptor}</th>
+                        {columns.map(columnTitle => (
+                            <th key={columnTitle} scope="col">{columnTitle}</th>
+                        ))}
+                    </tr>
+                </thead>
+                <tbody>
+                    {data.map((item, index) => (
+                        <tr key={item.id} className="table-row">
+                            <th scope="row">
+                                {++index}
+                            </th>
+                            {columns.map(columnTitle => (
+                                <td key={item[columnTitle] + columnTitle}>
+                                    {item[columnTitle]}
+                                </td>
+                            ))}
+                            <td className="btn-container">
+                                <Button onClick={() => onItemDelete(item.id)} label="Delete" classes='btn-danger'></Button>
+                                <Link to={{
+                                    pathname: `${url}/${item.id}`,
+                                    state: { ...item }
+                                }} style={{ margin: '0 10px' }}>
+                                    Edit
+                            </Link>
+                            </td>
+                        </tr>
+                    ))}
+                </tbody>
+            </table>)
             : <Alert variant="info">No data, but you can add some.</Alert>
         }
     </>)
