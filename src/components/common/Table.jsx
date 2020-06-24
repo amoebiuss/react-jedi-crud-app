d402b83c04eb28fe74f713b18d81ce531ec4d07c
Alena Vasilchenko <vasilchenko.alen@yandex.ru>
2020-06-25 00:18:29 +0300

    get rid of commented code

--- src/components/common/Table.jsx
+++ src/components/common/Table.jsx
@@ -1,6 +1,4 @@
 import React from 'react';
-import { nanoid } from 'nanoid';
-import { Link, useRouteMatch } from 'react-router-dom';
 import Button from './Button';
 import { Alert } from 'react-bootstrap';
 import { generateID } from '../../services/utils';
@@ -54,48 +52,5 @@ function Table({ columns, data, tableDescriptor, onItemDelete,}) {
         </table>
     )
 }
-
-// function Table({ columns, data, tableDescriptor, onItemDelete }) {
-//     let { url } = useRouteMatch();
-
-//     return (<>
-//         {data.length
-//             ? (<table className="table table-dark">
-//                 <thead>
-//                     <tr>
-//                         <th scope="col">{tableDescriptor}</th>
-//                         {columns.map(columnTitle => (
-//                             <th key={columnTitle} scope="col">{columnTitle}</th>
-//                         ))}
-//                     </tr>
-//                 </thead>
-//                 <tbody>
-//                     {data.map((item, index) => (
-//                         <tr key={item.id} className="table-row">
-//                             <th scope="row">
-//                                 {++index}
-//                             </th>
-//                             {columns.map(columnTitle => (
-//                                 <td key={item[columnTitle] + columnTitle}>
-//                                     {item[columnTitle]}
-//                                 </td>
-//                             ))}
-//                             <td className="btn-container">
-//                                 <Button onClick={() => onItemDelete(item.id)} label="Delete" classes='btn-danger'></Button>
-//                                 <Link to={{
-//                                     pathname: `${url}/${item.id}`,
-//                                     state: { ...item }
-//                                 }} style={{ margin: '0 10px' }}>
-//                                     Edit
-//                             </Link>
-//                             </td>
-//                         </tr>
-//                     ))}
-//                 </tbody>
-//             </table>)
-//             : <Alert variant="info">No data, but you can add some.</Alert>
-//         }
-//     </>)
-// }
 
 export default Table;
